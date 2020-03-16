31ef287a89b1d30d964c9c5e046b5355a2228042
sanopi <36667265+sanopi@users.noreply.github.com>
2020-03-17 00:51:05 +0900

    Initial Commit

---
+++ swiftui.tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui.tutorial
+//
+//  Created by 佐野友啓 on 2020/03/17.
+//  Copyright © 2020 sanopi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
