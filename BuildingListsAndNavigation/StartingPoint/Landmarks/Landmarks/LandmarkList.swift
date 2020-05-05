92ca2ae54895d1a66fdb9405ef140a49d65a763d
sanopi <36667265+sanopi@users.noreply.github.com>
2020-05-05 22:03:20 +0900

    Building Navigation

--- BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
+++ BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
@@ -2,8 +2,13 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarkData) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)){
+                    LandmarkRow(landmark: landmark)
+                }
+            
+            }.navigationBarTitle(Text("Landmarks"))
         }
     }
 }
