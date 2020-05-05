fbb75e3422b81146d813dc0bf226381560f18e0d
sanopi <36667265+sanopi@users.noreply.github.com>
2020-05-05 23:03:00 +0900

    Filter the List View by Toggling the State

--- HandlingUserInput/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
+++ HandlingUserInput/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
@@ -1,18 +1,27 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view showing a list of landmarks.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view showing a list of landmarks.
+ */
 
 import SwiftUI
 
 struct LandmarkList: View {
+    @State var showFavoriteOnly = true
+    
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoriteOnly) {
+                    Text("Favorites Only")
+                }
+                ForEach(landmarkData) { landmark in
+                    if !self.showFavoriteOnly || landmark.isFavorite {
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
             }
             .navigationBarTitle(Text("Landmarks"))
