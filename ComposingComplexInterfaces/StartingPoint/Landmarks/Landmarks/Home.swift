263f42787cf858c1d15e20525f09a088e279c5f3
sanopi <36667265+sanopi@users.noreply.github.com>
2020-07-08 00:01:41 +0900

    Categories List

---
+++ ComposingComplexInterfaces/StartingPoint/Landmarks/Landmarks/Home.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct CategoryHome: View {
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarkData,
+            by: { $0.category.rawValue }
+        )
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationBarTitle(Text("Featured"))
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
