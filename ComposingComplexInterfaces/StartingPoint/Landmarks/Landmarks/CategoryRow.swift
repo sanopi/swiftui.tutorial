a5d36186c72dd37ff17d4204190011870d1c675b
sanopi <36667265+sanopi@users.noreply.github.com>
2020-07-23 20:46:48 +0900

    Add Rows of Landmarks

---
+++ ComposingComplexInterfaces/StartingPoint/Landmarks/Landmarks/CategoryRow.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(self.categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 15)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(self.items) { landmark in
+                        Text(landmark.name)
+                        .font(.headline)
+                    }
+                }
+            }
+            
+        }.frame(height: 185)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarkData[0].category.rawValue,
+            items: Array(landmarkData.prefix(4))
+        )
+    }
+}
